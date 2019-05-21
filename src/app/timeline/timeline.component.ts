525a127664743ab9e1d75e5739be1a85ef75d75a
Cedric Moubri-Tournes <cedric.moubri-tournes@outlook.fr>
2019-05-21 17:54:48 +0200

    feat(timeline): reverse, hide future locations

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,28 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.scss']
 })
-export class TimelineComponent {
+export class TimelineComponent implements OnInit {
 
   events = [
     {
-      date: '18/09/2019',
-      state: 'future',
+      date: '06/06/2019',
+      state: 'past',
       duration: '1 mois',
-      country: 'Chine',
-      flagUrl: 'assets/images/flags/cn.svg',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc lorem, sagittis vitae mauris porttitor, efficitur'
-        + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
-    },
-    {
-      date: '06/08/2019',
-      state: 'future',
-      duration: '1 mois et demi',
-      country: 'Japon',
-      flagUrl: 'assets/images/flags/jp.svg',
+      country: 'Indonésie',
+      flagUrl: 'assets/images/flags/id.svg',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc lorem, sagittis vitae mauris porttitor, efficitur'
         + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
     },
@@ -36,16 +27,67 @@ export class TimelineComponent {
         + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
     },
     {
-      date: '06/06/2019',
-      state: 'past',
+      date: '06/08/2019',
+      state: 'future',
+      duration: '1 mois et demi',
+      country: 'Japon',
+      flagUrl: 'assets/images/flags/jp.svg',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc lorem, sagittis vitae mauris porttitor, efficitur'
+        + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
+    },
+    {
+      date: '18/09/2019',
+      state: 'future',
       duration: '1 mois',
-      country: 'Indonésie',
-      flagUrl: 'assets/images/flags/id.svg',
+      country: 'Chine',
+      flagUrl: 'assets/images/flags/cn.svg',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc lorem, sagittis vitae mauris porttitor, efficitur'
+        + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
+    },
+    {
+      date: '18/10/2019',
+      state: 'future',
+      duration: '1 mois',
+      country: 'Thailande',
+      flagUrl: 'assets/images/flags/th.svg',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc lorem, sagittis vitae mauris porttitor, efficitur'
+        + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
+    },
+    {
+      date: '18/11/2019',
+      state: 'future',
+      duration: '1 mois',
+      country: 'Vietnam',
+      flagUrl: 'assets/images/flags/vn.svg',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc lorem, sagittis vitae mauris porttitor, efficitur'
+        + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
+    },
+    {
+      date: '18/12/2019',
+      state: 'future',
+      duration: '1 mois',
+      country: 'Laos',
+      flagUrl: 'assets/images/flags/la.svg',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc lorem, sagittis vitae mauris porttitor, efficitur'
+        + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
+    },
+    {
+      date: '18/01/2019',
+      state: 'future',
+      duration: '1 mois et demi',
+      country: 'Inde',
+      flagUrl: 'assets/images/flags/in.svg',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nunc lorem, sagittis vitae mauris porttitor, efficitur'
         + ' fermentum ante. Quisque velit eros, ultricies sit amet eros quis, porta placerat mauris.'
     }
   ];
 
+  public isFutureLocationsCollapsed = true;
+  public collapseFutureLocationIndex = 0;
+
   constructor() { }
 
+  ngOnInit() {
+    this.collapseFutureLocationIndex = this.events.findIndex(event => event.state === 'future');
+  }
 }
